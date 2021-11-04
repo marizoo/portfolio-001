2facd759d02009e8bb25f6b21f62fe6f5d0c5f80
Marizoo <57407787+marizoo@users.noreply.github.com>
2021-11-04 16:42:42 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    img: "../../img/cnn.png",
+    link: "http://lama.dev",
+    alt: "image 1",
+  },
+  {
+    id: 2,
+    img: "",
+    link: "http://lama.dev",
+    alt: "image 2",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "http://lama.dev",
+    alt: "image 3",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "http://lama.dev",
+    alt: "image 4",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "http://lama.dev",
+    alt: "image 5",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "http://lama.dev",
+    alt: "image 6",
+  },
+];
